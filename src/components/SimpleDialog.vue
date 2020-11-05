b096a8daf9ceba5b269d0eed871f6070bb5dd549
N0irag <macgarion@gmail.com>
2020-11-05 11:49:19 +0100

    backdrop pour SimpleDialog

--- src/components/SimpleDialog.vue
+++ src/components/SimpleDialog.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="dialog-window" v-show="visible">
-    <header>
-      <h1>{{titre}}</h1>
-    </header>
-    <div class="main">
-      <slot/>
+  <div class="dialog-parent">
+
+    <div class="dialog-backdrop" v-show="visible"></div>
+
+    <div class="dialog-window" v-show="visible">
+      <header>
+        <h1>{{titre}}</h1>
+      </header>
+      <div class="main">
+        <slot/>
+      </div>
+      <footer>
+        <button @click="visible = false">Close</button>
+      </footer>
     </div>
-    <footer>
-      <button @click="visible = false">Close</button>
-    </footer>
+
   </div>
 </template>
 <script>
@@ -33,3 +39,27 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.dialog-window {
+  position: relative;
+  pointer-events: auto;
+}
+.dialog-parent {
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.dialog-parent, .dialog-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+.dialog-backdrop {
+  pointer-events: auto;
+  background-color: rgba(255,30,80,0.5);
+}
+</style>
